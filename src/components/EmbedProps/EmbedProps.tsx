89e68b7801fc08403357f593e1744c0d4b80f08c
Yuri Stepin <enchantinggg4@gmail.com>
2024-11-09 00:08:59 +0300

    Fix title react comment stuff

--- src/components/EmbedProps/EmbedProps.tsx
+++ src/components/EmbedProps/EmbedProps.tsx
@@ -11,10 +11,11 @@ interface IEmbedPropsProps {
 
 export const EmbedProps: React.FC<IEmbedPropsProps> = (p) => {
   const url = useRouter().asPath;
+  const title = `DOTA2CLASSIC - ${p.title}`;
   return (
     <Head>
-      <meta property="og:title" content={`DOTA2CLASSIC - ${p.title}`} />
-      <title>DOTA2CLASSIC - {p.title}</title>
+      <meta property="og:title" content={title} />
+      <title>{title}</title>
       <meta property="og:description" content={p.description} />
       <meta property="og:image" content={p.image} />
       <meta property="summary_large_image" content={p.image} />
