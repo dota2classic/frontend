7b6431d7ad13a1864c1c0f07cf56a8a1616bd3e5
Yuri Stepin <enchantinggg4@gmail.com>
2024-11-11 17:39:51 +0300

    Small fix again

--- src/components/Notifications/Notifications.tsx
+++ src/components/Notifications/Notifications.tsx
@@ -4,12 +4,13 @@ import c from "./Notifications.module.scss";
 import { observer } from "mobx-react-lite";
 import { NotificationDto } from "@/store/NotificationStore";
 import { useStore } from "@/store";
+import cx from "classnames";
 
 export const Notifications = observer(() => {
-  const { notify } = useStore();
+  const { notify, queue } = useStore();
 
   return (
-    <div className={c.container}>
+    <div className={cx(c.container, queue.gameInfo?.serverURL && c.plsMove)}>
       {notify.permanentQueue.map((t: NotificationDto) => (
         <div key={t.id}>{t.text}</div>
       ))}
