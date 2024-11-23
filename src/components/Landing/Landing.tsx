73d76618645ec644e96c1cd00274079aa57101c6
Yuri Stepin <enchantinggg4@gmail.com>
2024-11-23 21:27:32 +0300

    Landing fixes

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -1,6 +1,4 @@
-import React, { useEffect } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
+import React from "react";
 
 import c from "./Landing.module.scss";
 import { CoolList, Duration, EmbedProps, PageLink } from "@/components";
@@ -9,12 +7,6 @@ import cx from "clsx";
 import Image from "next/image";
 
 export const Landing = () => {
-  useEffect(() => {
-    if (typeof window !== "undefined")
-      AOS.init({
-        disable: "mobile",
-      });
-  }, []);
   return (
     <>
       <EmbedProps
@@ -35,13 +27,11 @@ export const Landing = () => {
             src={`/landing/d2video-new.webm`}
           />
         </div>
-        <div
-          className={c.leadingIntent}
-          data-aos="fade-right"
-          data-aos-delay="500"
-          data-aos-duration="1500"
-        >
-          <h1>Волшебный мир старой Доты</h1>
+        <div className={c.leadingIntent}>
+          <h1>Скучаешь по старой доте?</h1>
+          <p>
+            Начни играть в настоящую <span className="gold">Dota 2</span> онлайн
+          </p>
           <PageLink link={AppRouter.download.link} className={c.playButton}>
             Играть бесплатно
           </PageLink>
@@ -57,102 +47,116 @@ export const Landing = () => {
             alt=""
           />
         </div>
-        <div
-          className={c.wow}
-          data-aos="fade-up"
-          data-aos-delay="0"
-          data-aos-duration="1500"
-        >
-          <h2>Присоединяйся</h2>
-          <div>К тысячам игроков, которые предпочитают настоящую доту</div>
-        </div>
-      </div>
-      <div className={c.block}>
-        <div className={c.intent}>
-          <CoolList
-            items={[
-              {
-                title: "Скачать игровой клиент",
-                content: (
-                  <>
-                    Для игры в старую доту нужен старый клиент игры. Мы скачали
-                    и собрали его по кусочкам, чтобы тебе осталось только
-                    скачать архив с готовой игрой.{" "}
-                    <PageLink link={AppRouter.download.link} className="link">
-                      Скачать архив с игрой
-                    </PageLink>
-                  </>
-                ),
-              },
-              {
-                title: "Первый запуск игры",
-                content: (
-                  <>
-                    Запускать игру из архива не нужно: для этого сначала нужно
-                    распаковать архив. Распакуй в удобную тебе папку и запусти
-                    dota.bat.
-                  </>
-                ),
-              },
-              {
-                title: "Первый матч с ботами",
-                content: (
-                  <>
-                    Старая дота очень сильно отличается от современной версии:
-                    даже если ты опытный игрок, тебе будет очень непривычно. Мы
-                    рекомендуем сначала поиграть с ботами, чтобы настроить
-                    бинды, привыкнуть к старым героям, карте и способностям.
-                  </>
-                ),
-              },
-              {
-                title: "Первая онлайн игра",
-                content: (
-                  <>
-                    Для игры с людьми был создан этот сайт: ты не можешь просто
-                    нажать поиск в самом клиенте. В поиске через сайт нет ничего
-                    сложного, но перед игрой с другими игроками тебе нужно
-                    сыграть обучение. Это обычный матч с ботами, но на наших
-                    серверах. Тебе предстоит принять игру, подключить к игровому
-                    серверу и завершить матч.
-                  </>
-                ),
-              },
-            ]}
-          />
-
-          <h3>Кто мы такие</h3>
-          <div className={c.longtext}>
-            <ol>
-              <li>dota2classic - проект энтузиастов и фанатов старой Dota 2</li>
-              <li>
-                Проект существует <b className="gold">4 года</b>, за это время
-                более <b className="gold">14 тысяч игроков</b> сыграло более{" "}
-                <b className="gold">20 тысяч матчей</b> продолжительностью{" "}
-                <span className="gold">
-                  <Duration big duration={35130960} />
-                </span>
-              </li>
-              <li>
-                Весь исходный код можно найти на{" "}
-                <a
-                  target="__blank"
-                  href="https://github.com/dota2classic"
-                  className="link"
-                >
-                  Github
-                </a>
-              </li>
-            </ol>
+        <div className={c.wow}>
+          <h2>Попробуй оригинальную игру на вкус</h2>
+          <div>
+            Тысячи игроков предпочитают играть в{" "}
+            <span className="red">Dota 2</span> на сайте dotaclassic.ru
           </div>
         </div>
       </div>
-      <div
-        className={cx(c.block, c.carousel)}
-        // data-aos="fade-up"
-        // data-aos-delay="0"
-        // data-aos-duration="1500"
-      >
+
+      <div className={c.panel}>
+        <h3>Тебя ждет несколько простых шагов</h3>
+        <CoolList
+          items={[
+            {
+              title: "Скачать игровой клиент",
+              content: (
+                <>
+                  Для игры в старую доту нужен старый клиент игры. Мы скачали и
+                  собрали его по кусочкам, чтобы тебе осталось только скачать
+                  архив с готовой игрой.{" "}
+                  <PageLink link={AppRouter.download.link} className="link">
+                    Скачать архив с игрой
+                  </PageLink>
+                </>
+              ),
+            },
+            {
+              title: "Запуск игры",
+              content: (
+                <>
+                  Запускать игру из архива не нужно: для этого сначала нужно
+                  распаковать архив. Распакуй в удобную тебе папку и запусти
+                  dota.bat.
+                </>
+              ),
+            },
+            {
+              title: "Первый матч с ботами",
+              content: (
+                <>
+                  Старая дота очень сильно отличается от современной версии:
+                  даже если ты опытный игрок, тебе будет очень непривычно. Мы
+                  рекомендуем сначала поиграть с ботами, чтобы настроить бинды,
+                  привыкнуть к старым героям, карте и способностям.
+                </>
+              ),
+            },
+            {
+              title: "Первая онлайн игра",
+              content: (
+                <>
+                  Для игры с людьми был создан этот сайт: ты не можешь просто
+                  нажать поиск в самом клиенте. В поиске через сайт нет ничего
+                  сложного, но перед игрой с другими игроками тебе нужно сыграть
+                  обучение. Это обычный матч с ботами на наших серверах. Тебе
+                  предстоит принять игру, подключиться к игровому серверу и
+                  завершить матч.
+                </>
+              ),
+            },
+          ]}
+        />
+      </div>
+      <div className={c.panel}>
+        <h3>Кто мы такие</h3>
+        <CoolList
+          items={[
+            {
+              title: "dota2classic",
+              content: (
+                <>
+                  Проект энтузиастов и фанатов старой Dota 2. Мы начали играть в
+                  эту игру, и хотим продолжать в нее играть.
+                </>
+              ),
+            },
+            {
+              title: "Неужели это реальность?",
+              content: (
+                <>
+                  Проект существует <b className="gold">4 года</b>, за это время
+                  более <b className="gold">14 тысяч игроков</b> сыграло более{" "}
+                  <b className="gold">20 тысяч матчей</b> продолжительностью{" "}
+                  <span className="gold">
+                    <Duration big duration={35130960} />
+                  </span>
+                </>
+              ),
+            },
+            {
+              title: "Бесплатно?!",
+              content: (
+                <>
+                  Весь исходный код можно найти на{" "}
+                  <a
+                    target="__blank"
+                    href="https://github.com/dota2classic"
+                    className="link"
+                  >
+                    Github.
+                  </a>{" "}
+                  Проект некоммерческий, и главной его целью является
+                  предоставление возможности играть в любимую игру
+                </>
+              ),
+            },
+          ]}
+        />
+      </div>
+      <div className={cx(c.block, c.carousel)}>
         <PageLink link={AppRouter.players.leaderboard().link}>
           <h3>Лучшие игроки</h3>
           <Image
