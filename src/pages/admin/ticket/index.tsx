774bc6fbc3ce6c225653bdb8ef5c235ea7605b85
Yuri Stepin <enchantinggg4@gmail.com>
2025-02-04 14:24:47 +0300

    Fix?

--- src/pages/admin/ticket/index.tsx
+++ src/pages/admin/ticket/index.tsx
@@ -1,10 +1,11 @@
 import { getApi } from "@/api/hooks";
 import { numberOrDefault } from "@/util/urls";
 import { ThreadPageDTO, ThreadType } from "@/api/back";
-import { EmbedProps, Pagination, ThreadsTable } from "@/components";
+import {Button, EmbedProps, PageLink, Pagination, ThreadsTable} from "@/components";
 import { AppRouter } from "@/route";
 import React from "react";
 import { NextPageContext } from "next";
+import c from "@/pages/forum/Forum.module.scss";
 
 interface Props {
   threads: ThreadPageDTO;
@@ -18,6 +19,14 @@ export default function AdminTicketsPage({ threads, page }: Props) {
         title="Форум"
         description="Dota2Classic форум - место для обсуждения матчей, игроков, героев и прочих важных вопросов"
       />
+      <div className={c.buttons}>
+        <PageLink
+          link={AppRouter.forum.index(0).link}
+          className={c.createThread}
+        >
+          <Button>Темы</Button>
+        </PageLink>
+      </div>
       {threads.pages > 1 && (
         <Pagination
           page={page}
