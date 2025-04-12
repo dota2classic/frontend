dc73549a4731e1036f844654eb3f504be38b64b1
Yuri Stepin <enchantinggg4@gmail.com>
2025-04-12 23:10:44 +0300

    Navbar for admin

---
+++ src/components/Navbar/AdminNavbarItem.tsx
@@ -0,0 +1,35 @@
+import { NavbarItem } from "@/components";
+import { AppRouter } from "@/route";
+
+import c from "./Navbar.module.scss";
+
+export const AdminNavbarItem = () => {
+  return (
+    <NavbarItem
+      className={c.navbar__admin}
+      action={AppRouter.admin.queues.link}
+      options={[
+        {
+          label: "Сервера",
+          action: AppRouter.admin.servers.link,
+        },
+        {
+          label: "Нарушения",
+          action: AppRouter.admin.crimes().link,
+        },
+
+        {
+          newCategory: true,
+          label: "Ф:Настройки",
+          action: AppRouter.admin.feedback.index.link,
+        },
+        {
+          label: "Ф:Список",
+          action: AppRouter.admin.crimes().link,
+        },
+      ]}
+    >
+      Очереди
+    </NavbarItem>
+  );
+};
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -15,16 +15,14 @@ import { LoginProfileNavbarItem } from "@/components/Navbar/LoginProfileNavbarIt
 import { MetaNavbarItem } from "@/components/Navbar/MetaNavbarItem";
 import { MdForum } from "react-icons/md";
 import { IoMdPlay } from "react-icons/io";
-import { useRouter } from "next/router";
+import { AdminNavbarItem } from "@/components/Navbar/AdminNavbarItem";
 
 export const Navbar = observer(function Navbar(p: { className?: string }) {
   const { auth } = useStore();
   const { isAdmin, isModerator } = auth;
   const isAuthorized = auth.isAuthorized;
   const [menuOpen, setMenuOpen] = useState(false);
-  const router = useRouter();
 
-  const isAdminLayout = router.pathname.startsWith("/admin");
   const [changing] = useRouterChanging();
 
   useEffect(() => {
@@ -40,85 +38,64 @@ export const Navbar = observer(function Navbar(p: { className?: string }) {
 
   return (
     <div className={cx(c.navbar, p.className)}>
-      {((isAdmin || isModerator) && isAdminLayout && (
-        <div className={cx(c.navbarInner, c.navbar__admin)}>
-          <ul className={c.navbarList}>
-            <NavbarItem action={AppRouter.admin.servers.link}>
-              Сервера
-            </NavbarItem>
-            <NavbarItem action={AppRouter.admin.queues.link}>
-              Очереди
-            </NavbarItem>
-            <NavbarItem action={AppRouter.admin.crimes().link}>
-              Нарушения
-            </NavbarItem>
-            <NavbarItem action={AppRouter.admin.feedback.index.link}>
-              Фидбек: Настройки
-            </NavbarItem>
-            <NavbarItem action={AppRouter.admin.playerFeedback(0).link}>
-              Фидбек пользователей
-            </NavbarItem>
-          </ul>
-        </div>
-      )) || (
-        <div className={c.navbarInner}>
-          <ul className={c.navbarList}>
-            <NavbarItem className={c.root} action={AppRouter.index.link}>
-              <SiDota2 />
-              DOTA2CLASSIC
-            </NavbarItem>
-            <div className={cx(c.navbarList__desktop, menuOpen && c.visible)}>
-              {(isAuthorized && (
-                <NavbarItem
-                  action={AppRouter.queue.link}
-                  options={[
-                    {
-                      Icon: IoMdPlay,
-                      label: "Гайд",
-                      action: AppRouter.download.link,
-                    },
-                  ]}
-                >
-                  Играть
-                </NavbarItem>
-              )) || (
-                <NavbarItem action={AppRouter.download.link}>Гайд</NavbarItem>
-              )}
-
-              <MetaNavbarItem />
+      <div className={c.navbarInner}>
+        <ul className={c.navbarList}>
+          <NavbarItem className={c.root} action={AppRouter.index.link}>
+            <SiDota2 />
+            DOTA2CLASSIC
+          </NavbarItem>
+          <div className={cx(c.navbarList__desktop, menuOpen && c.visible)}>
+            {(isAuthorized && (
               <NavbarItem
+                action={AppRouter.queue.link}
                 options={[
                   {
-                    Icon: MdForum,
-                    action: AppRouter.forum.index().link,
-                    label: "Форум",
+                    Icon: IoMdPlay,
+                    label: "Гайд",
+                    action: AppRouter.download.link,
                   },
                 ]}
-                action={AppRouter.blog.index.link}
               >
-                Новости
+                Играть
               </NavbarItem>
-              {hasLiveMatches && (
-                <NavbarItem
-                  className={c.liveMatch}
-                  action={AppRouter.matches.live.link}
-                  tip={liveMatches?.length}
-                >
-                  Live
-                </NavbarItem>
-              )}
-              <div className={c.spacer} />
-              <LoginProfileNavbarItem />
-            </div>
+            )) || (
+              <NavbarItem action={AppRouter.download.link}>Гайд</NavbarItem>
+            )}
 
-            <div className={c.mobileMenu}>
-              <NavbarItem action={() => setMenuOpen((t) => !t)}>
-                <IoMenu />
+            <MetaNavbarItem />
+            <NavbarItem
+              options={[
+                {
+                  Icon: MdForum,
+                  action: AppRouter.forum.index().link,
+                  label: "Форум",
+                },
+              ]}
+              action={AppRouter.blog.index.link}
+            >
+              Новости
+            </NavbarItem>
+            {hasLiveMatches && (
+              <NavbarItem
+                className={c.liveMatch}
+                action={AppRouter.matches.live.link}
+                tip={liveMatches?.length}
+              >
+                Live
               </NavbarItem>
-            </div>
-          </ul>
-        </div>
-      )}
+            )}
+            {(isAdmin || isModerator) && <AdminNavbarItem />}
+            <div className={c.spacer} />
+            <LoginProfileNavbarItem />
+          </div>
+
+          <div className={c.mobileMenu}>
+            <NavbarItem action={() => setMenuOpen((t) => !t)}>
+              <IoMenu />
+            </NavbarItem>
+          </div>
+        </ul>
+      </div>
     </div>
   );
 });
