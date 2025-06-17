b3e7667db40969749a744e8bcc19404612242d33
Yuri Stepin <enchantinggg4@gmail.com>
2025-06-17 14:14:13 +0300

    Fix

--- src/util/getApiUrl.ts
+++ src/util/getApiUrl.ts
@@ -1,15 +1,15 @@
 export const getApiUrl = (): string => {
   if (typeof window !== "undefined") {
-    return window.API_URL;
+    return window.API_URL as string;
   } else {
-    return process.env.SERVER_API_URL;
+    return process.env.SERVER_API_URL as string;
   }
 };
 
-export const getSocketUrl = () => {
+export const getSocketUrl = (): string => {
   if (typeof window !== "undefined") {
-    return window.SOCKET_URL;
+    return window.SOCKET_URL as string;
   } else {
-    return process.env.SOCKET_URL;
+    return process.env.SOCKET_URL as string;
   }
 };
