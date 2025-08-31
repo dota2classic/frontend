e294d628f11c242370876dbfc1a4f45415cbc2d4
Yuri Stepin <enchantinggg4@gmail.com>
2025-09-01 02:57:31 +0300

    A

--- src/components/AdBlock/HorizontalAdBlock.tsx
+++ src/components/AdBlock/HorizontalAdBlock.tsx
@@ -11,18 +11,20 @@ export const HorizontalAdBlock: React.FC<Props> = ({}: Props) => {
   return (
     <div className={c.horizontalAdBlock}>
       {/*<GenericAdBlock bannerId={bannerId} />*/}
-      <img
-        className={cx(c.imgHorizontal, c.imgHorizontal__large)}
-        src="/ads/D2C_Collectors_Wide_01.webp"
-        loading="lazy"
-        alt=""
-      />
-      <img
-        className={cx(c.imgHorizontal, c.imgHorizontal__small)}
-        src="/ads/D2C_Collectors_Wide_02.webp"
-        loading="lazy"
-        alt=""
-      />
+      <a href="https://collectorsshop.ru/promo/old" target="__blank">
+        <img
+          className={cx(c.imgHorizontal, c.imgHorizontal__large)}
+          src="/ads/D2C_Collectors_Wide_01.webp"
+          loading="lazy"
+          alt=""
+        />
+        <img
+          className={cx(c.imgHorizontal, c.imgHorizontal__small)}
+          src="/ads/D2C_Collectors_Wide_02.webp"
+          loading="lazy"
+          alt=""
+        />
+      </a>
     </div>
   );
 };
