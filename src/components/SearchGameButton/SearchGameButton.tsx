10a4974c2c2392553703477a2dde2be08653de4b
y0nkers <y0nkerswastaken@gmail.com>
2026-01-01 18:59:01 +0300

    123

--- src/components/SearchGameButton/SearchGameButton.tsx
+++ src/components/SearchGameButton/SearchGameButton.tsx
@@ -1,8 +1,6 @@
 import { ReactNode } from "react";
 import { observer } from "mobx-react-lite";
 
-interface Props {
-  visible: boolean;
-  customContent?: ReactNode;
-}
-export const SearchGameButton = observer(() => { return null; });
+export const SearchGameButton = observer(() => {
+  return null;
+});
--- src/components/SearchGameFloater/SearchGameFloater.tsx
+++ src/components/SearchGameFloater/SearchGameFloater.tsx
@@ -3,17 +3,9 @@ import React from "react";
 import { AcceptGameModal } from "../AcceptGameModal";
 
 import c from "./SearchGameFloater.module.scss";
-import { useRouter } from "next/router";
 import { observer } from "mobx-react-lite";
-import { QueueGameState, useQueueState } from "@/util/useQueueState";
 
 export const SearchGameFloater: React.FC = observer(() => {
-  const router = useRouter();
-
-  const isQueuePage = router.pathname.startsWith("/queue");
-
-  const qState = useQueueState();
-
   return (
     <div className={c.container}>
       <AcceptGameModal />
