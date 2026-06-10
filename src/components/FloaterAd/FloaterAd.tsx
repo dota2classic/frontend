82db7e674d52cbb66b08e7e80635f2a639ea7ec8
Yuri Stepin <enchantinggg4@gmail.com>
2026-06-10 14:36:40 +0300

    Fixes

--- src/components/FloaterAd/FloaterAd.tsx
+++ src/components/FloaterAd/FloaterAd.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import c from "./FloaterAd.module.scss";
 import cx from "clsx";
@@ -6,6 +6,11 @@ import { threadFont } from "@/const/fonts";
 import { IoMdClose } from "react-icons/io";
 import { observer } from "mobx-react-lite";
 import { useStore } from "@/store";
+import { metrika } from "@/ym";
+
+const COLLECTORSHOP_AD_SHOWN = "COLLECTORSHOP_AD_SHOWN";
+const COLLECTORSHOP_AD_HIDDEN = "COLLECTORSHOP_AD_HIDDEN";
+const COLLECTORSHOP_AD_CLICK = "COLLECTORSHOP_AD_CLICK";
 
 export const FloaterAd: React.FC = observer(() => {
   const [isVisible, setIsVisible] = useState(true);
@@ -13,11 +18,18 @@ export const FloaterAd: React.FC = observer(() => {
   const isOld = useStore().auth.isOld;
   // const isOld = false;
 
+  useEffect(() => {
+    if (isOld || !isVisible) return;
+
+    metrika("reachGoal", COLLECTORSHOP_AD_SHOWN);
+  }, [isOld, isVisible]);
+
   const close = useCallback(
     (e: React.MouseEvent) => {
       e.stopPropagation();
       e.preventDefault();
       if (!isVisible) return;
+      metrika("reachGoal", COLLECTORSHOP_AD_HIDDEN);
       setIsVisible(false);
       setTimeout(() => setIsVisible(true), 60_000);
     },
@@ -35,6 +47,7 @@ export const FloaterAd: React.FC = observer(() => {
         threadFont.className,
         !isVisible && c.hidden,
       )}
+      onClick={() => metrika("reachGoal", COLLECTORSHOP_AD_CLICK)}
     >
       <img className={c.img} src="/collectors.webp" alt="" />
       <div className={c.text}>
